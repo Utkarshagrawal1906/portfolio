932a435409acb715758e864b4b67e771996cff35
utkarsh <utkarshagofficial@gmail.com>
2026-01-05 00:04:57 +0530

    sixth

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/portfolio/', // make assets load correctly when hosted under GitHub Pages
   plugins: [react()],
   base: '/portfolio/', // THIS IS KEY: It must match your repo name
 })
